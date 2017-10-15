88c811f4cead8917a30ce3c7b99953845550cd87
carloscituc <carlos.cituc@gmail.com>
2017-10-15 05:36:44 -0500

    Nuevo base

---
+++ Versión 1.9/carrilloOfrece_v.1.9_sr.sql
@@ -0,0 +1,427 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema u953457282_co
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema u953457282_co
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `u953457282_co` DEFAULT CHARACTER SET utf8 ;
+USE `u953457282_co` ;
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`categoria_producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`categoria_producto` (
+  `id_categoria_producto` INT(4) NOT NULL AUTO_INCREMENT,
+  `cat_pro_nombre` VARCHAR(30) CHARACTER SET 'utf8' NOT NULL,
+  PRIMARY KEY (`id_categoria_producto`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`usuario` (
+  `id_usuario` INT(4) NOT NULL AUTO_INCREMENT,
+  `usu_nombre` VARCHAR(50) NOT NULL,
+  `usu_contrasenia` VARCHAR(35) CHARACTER SET 'utf8' NOT NULL,
+  `usu_tipo` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id_usuario`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`persona`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`persona` (
+  `id_persona` INT(4) NOT NULL AUTO_INCREMENT,
+  `per_nombre` VARCHAR(35) NOT NULL,
+  `per_apellido` VARCHAR(40) NOT NULL,
+  `per_correo` VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
+  `per_correo_visible` TINYINT(1) NOT NULL,
+  `per_telefono` VARCHAR(10) NULL,
+  `per_telefono_visible` TINYINT(1) NULL,
+  `per_red_social` VARCHAR(60) NULL,
+  `per_red_social_visible` TINYINT(1) NOT NULL,
+  `id_usuario` INT(4) NOT NULL,
+  PRIMARY KEY (`id_persona`),
+  INDEX `fk_personal_usuario1_idx` (`id_usuario` ASC),
+  CONSTRAINT `fk_personal_usuario1`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `u953457282_co`.`usuario` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`producto` (
+  `id_producto` INT(4) NOT NULL AUTO_INCREMENT,
+  `pro_nombre` VARCHAR(45) NOT NULL,
+  `pro_descripcion_breve` VARCHAR(150) NOT NULL,
+  `pro_descripcion_general` VARCHAR(1000) NOT NULL,
+  `pro_precio` DECIMAL(7,2) NOT NULL,
+  `pro_fecha_publicacion` DATE NOT NULL,
+  `pro_estatus` TINYINT(1) NOT NULL,
+  `id_categoria_producto` INT(4) NOT NULL,
+  `id_persona` INT(4) NOT NULL,
+  PRIMARY KEY (`id_producto`),
+  INDEX `fk_producto_categoria_idx` (`id_categoria_producto` ASC),
+  INDEX `fk_producto_persona1_idx` (`id_persona` ASC),
+  CONSTRAINT `fk_producto_categoria`
+    FOREIGN KEY (`id_categoria_producto`)
+    REFERENCES `u953457282_co`.`categoria_producto` (`id_categoria_producto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_producto_persona1`
+    FOREIGN KEY (`id_persona`)
+    REFERENCES `u953457282_co`.`persona` (`id_persona`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`categoria_negocio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`categoria_negocio` (
+  `id_categoria_negocio` INT(4) NOT NULL AUTO_INCREMENT,
+  `cat_neg_nombre` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`id_categoria_negocio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`colonia_negocio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`colonia_negocio` (
+  `id_colonia_negocio` INT(4) NOT NULL AUTO_INCREMENT,
+  `direc_colonia` VARCHAR(20) NOT NULL,
+  `direc_codigo_postal` VARCHAR(5) NOT NULL,
+  PRIMARY KEY (`id_colonia_negocio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`negocio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`negocio` (
+  `id_negocio` INT(4) NOT NULL AUTO_INCREMENT,
+  `neg_nombre` VARCHAR(45) NOT NULL,
+  `neg_coordenadas` VARCHAR(25) NOT NULL,
+  `neg_telefono` VARCHAR(10) NULL,
+  `neg_fecha_publicacion` DATE NOT NULL,
+  `neg_descripcion_breve` VARCHAR(150) NOT NULL,
+  `neg_descripcion_general` VARCHAR(1000) CHARACTER SET 'utf8' NOT NULL,
+  `neg_correo` VARCHAR(50) NULL,
+  `neg_calle` VARCHAR(30) NOT NULL,
+  `neg_cruzamiento` VARCHAR(30) NOT NULL,
+  `neg_referencia` VARCHAR(100) NOT NULL,
+  `neg_red_social` VARCHAR(60) NULL,
+  `neg_img_busqueda` INT(1) NOT NULL,
+  `id_categoria_negocio` INT(4) NOT NULL,
+  `id_persona` INT(4) NOT NULL,
+  `id_colonia_negocio` INT(4) NOT NULL,
+  PRIMARY KEY (`id_negocio`),
+  INDEX `fk_negocio_categorias_negocios1_idx` (`id_categoria_negocio` ASC),
+  INDEX `fk_negocio_persona1_idx` (`id_persona` ASC),
+  INDEX `fk_negocio_direccion1_idx` (`id_colonia_negocio` ASC),
+  CONSTRAINT `fk_negocio_categorias_negocios1`
+    FOREIGN KEY (`id_categoria_negocio`)
+    REFERENCES `u953457282_co`.`categoria_negocio` (`id_categoria_negocio`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_negocio_persona1`
+    FOREIGN KEY (`id_persona`)
+    REFERENCES `u953457282_co`.`persona` (`id_persona`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_negocio_direccion1`
+    FOREIGN KEY (`id_colonia_negocio`)
+    REFERENCES `u953457282_co`.`colonia_negocio` (`id_colonia_negocio`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`categoria_servicio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`categoria_servicio` (
+  `id_categoria_servicio` INT(4) NOT NULL AUTO_INCREMENT,
+  `cat_ser_nombre` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`id_categoria_servicio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`colonia_servicio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`colonia_servicio` (
+  `id_colonia_servicio` INT(4) NOT NULL AUTO_INCREMENT,
+  `direc_colonia` VARCHAR(20) NOT NULL,
+  `direc_codigo_postal` VARCHAR(5) NOT NULL,
+  PRIMARY KEY (`id_colonia_servicio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`servicio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`servicio` (
+  `id_servicio` INT(4) NOT NULL AUTO_INCREMENT,
+  `ser_nombre` VARCHAR(45) NOT NULL,
+  `ser_coordenadas` VARCHAR(25) NOT NULL,
+  `ser_telefono` VARCHAR(10) NULL,
+  `ser_descripcion_breve` VARCHAR(150) NOT NULL,
+  `ser_descripcion_general` VARCHAR(1000) NOT NULL,
+  `ser_fecha_publicacion` DATE NOT NULL,
+  `ser_calle` VARCHAR(30) NOT NULL,
+  `ser_cruzamiento` VARCHAR(30) NOT NULL,
+  `ser_referencia` VARCHAR(100) NOT NULL,
+  `ser_red_social` VARCHAR(45) NULL,
+  `id_categoria_servicio` INT(4) NOT NULL,
+  `id_persona` INT(4) NOT NULL,
+  `id_colonia_servicio` INT(4) NOT NULL,
+  PRIMARY KEY (`id_servicio`),
+  INDEX `fk_servicios_categoria_servicios1_idx` (`id_categoria_servicio` ASC),
+  INDEX `fk_servicios_persona1_idx` (`id_persona` ASC),
+  INDEX `fk_servicios_direccion_servicio1_idx` (`id_colonia_servicio` ASC),
+  CONSTRAINT `fk_servicios_categoria_servicios1`
+    FOREIGN KEY (`id_categoria_servicio`)
+    REFERENCES `u953457282_co`.`categoria_servicio` (`id_categoria_servicio`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_servicios_persona1`
+    FOREIGN KEY (`id_persona`)
+    REFERENCES `u953457282_co`.`persona` (`id_persona`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_servicios_direccion_servicio1`
+    FOREIGN KEY (`id_colonia_servicio`)
+    REFERENCES `u953457282_co`.`colonia_servicio` (`id_colonia_servicio`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`denuncia_producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`denuncia_producto` (
+  `id_denuncia_producto` INT(4) NOT NULL AUTO_INCREMENT,
+  `den_motivo` VARCHAR(100) NOT NULL,
+  `den_fecha` DATE NOT NULL,
+  `id_producto` INT(4) NOT NULL,
+  PRIMARY KEY (`id_denuncia_producto`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`fecha`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`fecha` (
+  `fec_fecha` DATE NOT NULL)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`administrador`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`administrador` (
+  `id_administrador` INT(2) NOT NULL AUTO_INCREMENT,
+  `admin_usuario` VARCHAR(35) NOT NULL,
+  `admin_contrasenia` VARCHAR(20) NOT NULL,
+  `admin_tipo` TINYINT(1) NOT NULL,
+  PRIMARY KEY (`id_administrador`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`notificacion`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`notificacion` (
+  `id_notificacion` INT(4) NOT NULL AUTO_INCREMENT,
+  `not_nombre_persona` VARCHAR(35) NULL,
+  `not_correo` VARCHAR(50) NOT NULL,
+  `not_fecha` DATE NOT NULL,
+  `id_producto` INT(4) NOT NULL,
+  PRIMARY KEY (`id_notificacion`),
+  INDEX `fk_notificacion_producto1_idx` (`id_producto` ASC),
+  CONSTRAINT `fk_notificacion_producto1`
+    FOREIGN KEY (`id_producto`)
+    REFERENCES `u953457282_co`.`producto` (`id_producto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`horario_negocio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`horario_negocio` (
+  `id_negocio` INT(4) NOT NULL,
+  `hor_dia` VARCHAR(9) NOT NULL,
+  `hor_corte1_apertura` TIME NOT NULL,
+  `hor_corte1_cierre` TIME NOT NULL,
+  `hor_corte2_apertura` TIME NULL,
+  `hor_corte2_cierre` TIME NULL)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`horario_servicio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`horario_servicio` (
+  `id_servicio` INT(4) NOT NULL,
+  `hor_dia` VARCHAR(9) NOT NULL,
+  `hor_corte1_apertura` TIME NOT NULL,
+  `hor_corte1_cierre` TIME NOT NULL,
+  `hor_corte2_apertura` TIME NULL,
+  `hor_corte2_cierre` TIME NULL)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`calificacion_negocio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`calificacion_negocio` (
+  `cal_puntaje` INT(2) NOT NULL,
+  `id_usuario` INT(4) NOT NULL,
+  `id_negocio` INT(4) NULL,
+  PRIMARY KEY (`id_usuario`,`id_negocio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`calificacion_servicio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`calificacion_servicio` (
+  `cal_puntaje` INT(2) NOT NULL,
+  `id_usuario` INT(4) NOT NULL,
+  `id_servicio` INT(4) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_servicio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`calificacion_usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`calificacion_usuario` (
+  `cal_puntaje` INT(2) NOT NULL,
+  `id_persona` INT(4) NOT NULL,
+  `id_usuario` INT(4) NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_persona`))
+
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`denuncia_negocio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`denuncia_negocio` (
+  `id_denuncia_negocio` INT(4) NOT NULL AUTO_INCREMENT,
+  `den_motivo` VARCHAR(100) NOT NULL,
+  `den_fecha` DATE NOT NULL,
+  `id_negocio` INT(4) NOT NULL,
+  PRIMARY KEY (`id_denuncia_negocio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`denuncia_servicio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`denuncia_servicio` (
+  `id_denuncia_servicio` INT NOT NULL AUTO_INCREMENT,
+  `den_motivo` VARCHAR(100) NOT NULL,
+  `den_fecha` DATE NOT NULL,
+  `id_servicio` INT(4) NOT NULL,
+  PRIMARY KEY (`id_denuncia_servicio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`publicidad_producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`publicidad_producto` (
+  `id_publicidad_producto` INT(4) NOT NULL AUTO_INCREMENT,
+  `pub_monto_pagado` DECIMAL(6,2) NOT NULL,
+  `pub_imagen` VARCHAR(4) NOT NULL,
+  `pub_meses` INT(2) NOT NULL,
+  `pub_fecha` DATE NULL,
+  `id_producto` INT(4) NOT NULL,
+  PRIMARY KEY (`id_publicidad_producto`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`publicidad_negocio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`publicidad_negocio` (
+  `id_publicidad_negocio` INT(4) NOT NULL AUTO_INCREMENT,
+  `pub_monto_pagado` DECIMAL(6,2) NOT NULL,
+  `pub_imagen` VARCHAR(4) NOT NULL,
+  `pub_meses` INT(2) NOT NULL,
+  `pub_fecha` DATE NULL,
+  `id_negocio` INT(4) NOT NULL,
+  PRIMARY KEY (`id_publicidad_negocio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`publicidad_servicio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`publicidad_servicio` (
+  `id_publicidad_servicio` INT(4) NOT NULL AUTO_INCREMENT,
+  `pub_monto_pagado` DECIMAL(6,2) NOT NULL,
+  `pub_imagen` VARCHAR(4) NOT NULL,
+  `pub_meses` INT(2) NOT NULL,
+  `pub_fecha` DATE NULL,
+  `id_servicio` INT(4) NOT NULL,
+  PRIMARY KEY (`id_publicidad_servicio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`compra`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`compra` (
+  `id_compra` INT(4) NOT NULL AUTO_INCREMENT,
+  `articulos_total` INT(2) NOT NULL,
+  `precio_total` DECIMAL(7,2) NOT NULL,
+  `fecha_compra` DATE NOT NULL,  
+  `id_usuario` INT(4) NOT NULL,
+  PRIMARY KEY (`id_compra`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `u953457282_co`.`articulo_compra`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `u953457282_co`.`articulo_compra` (
+  `id_articulo_compra` INT(4) NOT NULL AUTO_INCREMENT,
+  `id_articulo` VARCHAR(30) NOT NULL,
+  `id_articulo_repetido` INT(4) NOT NULL,
+  `art_nombre` VARCHAR(45) NOT NULL,
+  `cantidad_mes` INT(2) NOT NULL,  
+  `costo_mes` DECIMAL(7,2) NOT NULL,
+  `tipo_articulo` VARCHAR(10) NOT NULL,
+  `total` DECIMAL(7,2) NOT NULL,
+  `fecha_inicio` DATE NOT NULL,
+  `fecha_fin` DATE NOT NULL,
+  `id_compra` INT(4) NOT NULL,
+  PRIMARY KEY (`id_articulo_compra`))
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
